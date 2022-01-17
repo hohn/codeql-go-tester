eab53110ab49800665286e32142d42bfbbf2ad83
s-samadi <s-samadi@github.com>
2022-01-17 21:30:35 +1100

    Updated query

--- main.go
+++ main.go
@@ -32,6 +32,22 @@ func AuthorizationMiddleware(next http.Handler) http.Handler {
 	})
 }
 
+
+func AuthorizationMiddleware_Bad(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		profile := r.Header.Get("Authorization")
+		if len(profile) == 0 {
+			fmt.Println("missing auth token")
+			rw.WriteHeader(401)
+			return
+		}
+		tokenID := mux.Vars(r)["id"]
+		next.ServeHTTP(rw, r)
+	})
+}
+
+
+
 func GetAccount(rw http.ResponseWriter, r *http.Request) {
 	io.WriteString(rw, `{"message": "hello world.."}`)
 }
@@ -69,6 +85,24 @@ func main() {
 	fmt.Println("running...")
 	router := mux.NewRouter()
 	router.Handle("/account/{id}", AuthorizationMiddleware(http.HandlerFunc(GetAccount)))
+	http.Handle("/", router)
+	http.ListenAndServe(":8000", router)
+}
+
+
+func main_bad2() {
+	fmt.Println("running...")
+	router := mux.NewRouter()
+	/* signature expected by Handle:
+	Handle(path string, handler http.Handler) *Route, so
+	we always have a HandlerFunc()
+	*/
+	router.Handle("/account/{id}",  AuthorizationMiddleware_Bad(http.HandlerFunc(GetAccount)))
+	/*
+		cannot use GetAccount (value of type func(rw http.ResponseWriter, r *http.Request)) as http.Handler value in argument to router.Handle: missing method
+
+		router.Handle("/account/{id}", GetAccount)
+	*/
 	http.Handle("/", router)
 	http.ListenAndServe(":8000", router)
 }
